289bdcd21704fd79799d029ed5e4226a0e7c9520
noahdsaria <noahdsaria@gmail.com>
2025-05-24 15:16:45 -0500

    Adjusted Message + Featured sections

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -4,30 +4,57 @@ import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.headerText} textAlign='left'>Messages</Text>
+      <Text style={styles.headerText} textAlign='left'>Latest Message</Text>
       <MessageSection />
+      <Text style={styles.headerText} textAlign='left'>More from CyLife</Text>
+      <FeaturedSection />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
 
+// Note: For now, display latest message recording, might add dynamic tab for livestream later
 function MessageSection() {
   return (
     <View>
-        <ScrollView horizontal={true} 
+      <ScrollView horizontal={true} 
         style={styles.messageSection} 
         showsHorizontalScrollIndicator={false}
-        >
+        contentContainerStyle={{ 
+          justifyContent: 'center',
+          alignItems: 'center', 
+          flexGrow: 1,
+        }}
+      >
           {/* Example message items */} 
           <View style={styles.messageBox}>
-            <Text>Message 1</Text>
+            <Text>Recorded Video</Text>
           </View>
-          <View style={styles.messageBox}>
-            <Text>Message 2</Text>
-          </View>
-          <View style={styles.messageBox}>
-            <Text>Message 3</Text>
-          </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+function FeaturedSection() {
+  return (
+    <View>
+      <ScrollView horizontal={true} 
+        style={styles.featuredSection} 
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ alignItems: 'center' }}
+      >
+        <View style={styles.featuredBox}>
+          <Text>News 1</Text>
+        </View>
+
+        <View style={styles.featuredBox}>
+          <Text>News 2</Text>
+        </View>
+
+        <View style={styles.featuredBox}>
+          <Text>News 3</Text>
+        </View>
+
       </ScrollView>
     </View>
   );
@@ -37,28 +64,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginTop: 0,
   },
 
   headerText: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     marginLeft: 10,
-    paddingLeft: 10,
+    marginBottom: 0,
   },
 
   messageSection: {
     flexDirection: 'row',
-    padding: 10,
-    paddingLeft: 20,
+    flex: 1,
     backgroundColor: '#fff',
-    borderRadius: 10,
-    margin: 0,
+    margin: 10,
   },
 
   messageBox : {
-    width: 250,
+    flex: 1,
     height: 200,
     backgroundColor: 'lightgray',
+    borderRadius: 15,
+  },
+
+  featuredSection : {
+    flexDirection: 'row',
+    flex: 1,
+    backgroundColor: '#fff',
+    margin: 10,
+  },
+
+  featuredBox : {
+    width: 200,
+    height: 200,
+    backgroundColor: 'lightgray',
+    borderRadius: 15,
     marginRight: 10,
   },
 
